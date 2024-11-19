7e7978f9b449acf2c416ce3739f93cdba79b8f74
LaBasura <joeccoon@gmail.com>
2024-11-18 22:10:08 -0800

    Links Added

--- src/app/components/Home_Card.tsx
+++ src/app/components/Home_Card.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 interface props {
   image: string;
   name: string;
 }
 const HomeCard: React.FC<props> = ({ image, name }: props) => {
   return (
-    <div className="relative flex flex-col w-4/12 h-96 mx-5 text-center pt-8">
+    <Link
+      href="/test"
+      className="relative flex flex-col w-1/3 h-[calc(50vh)] mx-5 text-center pt-8 shadow-lg shadow-slate-950"
+    >
       <span className="relative text-4xl font-semibold font-serif mb-9">
         {name}
       </span>
-      <div className="relative flex-grow">
-        <Image src={image} alt={name} fill className="relative" />
+      <div className="relative h-full w-full">
+        <Image src={image} alt={name} fill />
       </div>
-    </div>
+    </Link>
   );
 };
 export default HomeCard;
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,7 +7,10 @@ const Navbar: React.FC = () => {
       <a className=" bg-red-700 ml-40" href="#">
         <Image src="/KwestIcon.jpeg" width={300} height={100} alt="facebook" />
       </a>
-      <a href="#" className="pt-3 rounded-full ml-auto mr-3">
+      <a
+        href="https://www.facebook.com/kwestflooringservices/"
+        className="pt-3 rounded-full ml-auto mr-3"
+      >
         <Image
           src="/FacebookIcon.png"
           width={width}
@@ -15,29 +18,28 @@ const Navbar: React.FC = () => {
           alt="facebook"
         />
       </a>
-      <a href="#" className="pt-3 rounded-full mr-3">
+      <a
+        href="https://www.instagram.com/kwestflooring/"
+        className="pt-3 rounded-full mr-3"
+      >
         <Image
           src="/InstagramIcon.png"
           width={width}
           height={height}
-          alt="facebook"
+          alt="instagram"
         />
       </a>
-      <a href="#" className="pt-3 rounded-full mr-3">
-        <Image
-          src="/YelpIcon.png"
-          width={width}
-          height={height}
-          alt="facebook"
-        />
+      <a
+        href="https://www.yelp.com/biz/kwest-flooring-services-san-diego-2"
+        className="pt-3 rounded-full mr-3"
+      >
+        <Image src="/YelpIcon.png" width={width} height={height} alt="yelp" />
       </a>
-      <a href="#" className="pt-3 rounded-full mr-56">
-        <Image
-          src="/MailIcon.png"
-          width={width}
-          height={height}
-          alt="facebook"
-        />
+      <a
+        href="mailto:kwestflooringservices@gmail.com"
+        className="pt-3 rounded-full mr-56"
+      >
+        <Image src="/MailIcon.png" width={width} height={height} alt="mail" />
       </a>
     </nav>
   );
